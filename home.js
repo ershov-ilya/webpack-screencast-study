8ebc7d78e8bad93d9ad1b393f6256d5eda989fdf
ILYA ERSHOV <ershov.ilya@gmail.com>
2017-01-30 11:09:39 +0300

    2.1 простой конфиг

---
+++ home.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * Website:  http://ershov.pw/ (RU/ENG)
+ * Date: 30.01.2017
+ * Time: 11:05
+ */
+
+let welcome = require('./welcome');
+
+welcome("home");
---
+++ welcome.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * Website:  http://ershov.pw/ (RU/ENG)
+ * Date: 30.01.2017
+ * Time: 11:06
+ */
+
+module.exports = function(message){
+    alert(`Welcome ${message}`);
+};
