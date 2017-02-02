d21e7a9784615ab914a2228a7d0578ea49130dc0
ILYA ERSHOV <ershov.ilya@gmail.com>
2017-02-02 13:47:37 +0300

    webpack config watch

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ module.exports={
     entry: "./home.es6",
     output: {
         //path: __dirname + "/dist",
-        filename: "build.js"
-    }
+        filename: "build.js",
+        library: "home"
+    },
+
+    watch: true,
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: "source-map"
 };
